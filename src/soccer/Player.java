b68c6faef820c1938bcef641abfb7099fd8b4aaf
Ali Ahmed Warsame <92198257+ly786691@users.noreply.github.com>
2022-06-27 08:42:29 +0300

    Update Player.java

--- src/soccer/Player.java
+++ src/soccer/Player.java
@@ -1,6 +1,22 @@
-
 package soccer;
 
+
 public class Player {
-    public String playerName; 
+    private String playerName;
+    
+    public Player () {};
+    public Player (String playerName){
+        this.playerName = playerName;
+        }
+
+   
+    public String getPlayerName() {
+        return playerName;
+    }
+
+   
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+    
 }
